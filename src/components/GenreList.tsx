32285116577a7ff5a9efb152156e83147b1d40c6
Yeskendir Sultanov <yeskendir.sultanov@gmail.com>
2023-07-22 23:05:47 +0600

    Fetch the genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,16 @@
+import React, { useDebugValue } from "react";
+import useGenres from "../hooks/useGenres";
+
+const GenreList = () => {
+  const { genres } = useGenres();
+  return (
+    <ul>
+      {genres.map((genre) => (
+        <li key={genre.id}> {genre.name} </li>
+      ))}
+      ;
+    </ul>
+  );
+};
+
+export default GenreList;
