654934b32714e77b6477bff78452a64075b0bc21
Yeskendir Sultanov <yeskendir.sultanov@gmail.com>
2023-07-23 00:37:47 +0600

    Build platform selector

---
+++ src/hooks/usePlatforms.ts
@@ -0,0 +1,11 @@
+import useData from "./useData";
+
+interface Platform {
+    id: number;
+    name: number;
+    slug: number;
+}
+
+const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+
+export default usePlatforms;
