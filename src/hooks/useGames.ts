f377734f40af804ddbec52e1378eb0ed1c2a28dd
Yeskendir Sultanov <yeskendir.sultanov@gmail.com>
2023-07-23 01:20:50 +0600

    Refactor: Extracting a Query Object

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,3 +1,4 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
 import { Genre } from "./useGenres";
 import { Platform } from "./usePlatforms";
@@ -10,13 +11,10 @@ export interface Game {
     metacritic: number;
   }
  
-const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => useData<Game>('/games', {
+const useGames = (gameQuery: GameQuery) => useData<Game>('/games', {
     params: {
-        genres: selectedGenre?.id, 
-        platforms: selectedPlatform?.id
-    }}, [
-        selectedGenre?.id, 
-        selectedPlatform?.id
-    ]);
+        genres: gameQuery.genre?.id, 
+        platforms: gameQuery.platform?.id
+    }}, [gameQuery]);
     
 export default useGames;
